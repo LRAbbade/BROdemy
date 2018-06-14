9df2b4b673514a1a702dd80624dc575bd1be7894
Lucas Abbade <lucasabbade@hotmail.com>
2018-06-14 12:07:10 -0300

    fixed bug with user courses display

--- app/controllers/profile/myCourse.js
+++ app/controllers/profile/myCourse.js
@@ -3,8 +3,6 @@ module.exports.show = function (application, req, res) {
     const userDAO = new application.app.models.UserDAO(connection);
 
     userDAO.showMyCourses(req.session.data, function (result) {
-        console.log(result);
         res.render('profile/mycourse', {inform: result, user: req.session.data,manage: false});
     });
 };
-
